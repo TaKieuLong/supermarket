5b2d7bfd51be612e84fe7ef55ca66765d5458045
TaKieuLong <takieulong@gmail.com>
2024-07-26 21:04:39 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -6,19 +6,32 @@ const typeDefs = require("./schema.js");
 const resolvers = require("./resolvers.js");
 const restRouter = require("./routes/index.js");
 const corsMiddleware = require("../src/middleware/corsMiddleware.js");
-const app = express();
-app.use(corsMiddleware);
-app.use(express.json());
 
-require("./mongodb.js");
+const app = express();
+
+// Sử dụng middleware CORS
+app.use(corsMiddleware);
+
+// Sử dụng các middleware khác
+app.use(express.json());
 app.use(bodyParser.json());
+
+// Kết nối MongoDB
+require("./mongodb.js");
+
+// Định nghĩa route cho REST API
 app.use("/api", restRouter);
 
+// Khởi động Apollo Server
 const server = new ApolloServer({ typeDefs, resolvers });
-await server.start().then(() => {
+
+const startServer = async () => {
+  await server.start();
   server.applyMiddleware({ app });
 
   app.listen({ port: 4000 }, () => {
     console.log("Server is running on port 4000");
   });
-});
+};
+
+startServer();
--- src/middleware/corsMiddleware.js
+++ src/middleware/corsMiddleware.js
@@ -1,9 +1,6 @@
-const cors = require("cors");
-
 const corsOptions = {
-  origin: "*",
+  origin: ["https://supermarket-5bkc.onrender.com", "http://localhost:4000"], // Thay đổi thành các nguồn gốc cụ thể nếu cần
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
-
-module.exports = cors(corsOptions);
