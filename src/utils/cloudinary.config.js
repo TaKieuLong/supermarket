fab1f62e90a3b81ae4a7d0dc76435b98926b73bc
TaKieuLong <takieulong@gmail.com>
2024-07-27 12:19:26 +0700

    clg check env

--- src/utils/cloudinary.config.js
+++ src/utils/cloudinary.config.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
+
+console.log("Cloudinary Config:");
+console.log("Cloud Name:", process.env.CLOUD_NAME);
+console.log("API Key:", process.env.CLOUD_API_KEY);
+console.log("API Secret:", process.env.CLOUD_API_SECRET);
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
